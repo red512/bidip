b4afb43067545d4485b8b877f13eb85ed8146e51
red512 <robojinx1@gmail.com>
2023-08-28 23:00:10 +0300

    editing react tests

--- fe-react/src/App.test.js
+++ fe-react/src/App.test.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders IP Address and Message', () => {
+  // Render the component
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // Assert that the expected text is present in the component
+  const ipAddressElement = screen.getByText(/IP Address:/i);
+  expect(ipAddressElement).toBeInTheDocument();
+
+  const messageElement = screen.getByText(/Message:/i);
+  expect(messageElement).toBeInTheDocument();
 });
